ad5c4785716982db7667b933a1ddda973e03e7a9
Sporas Laszlo <lackuszzz@freemail.hu>
2011-09-29 22:44:07 +0200

    Elso

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elso::Application.config.secret_token = 'b64285a267e87588588253cb0582757f9235ea94f6972336e1f99425462d17761af23e9187ad44b789e651647bab09ad576d9441edc4a3e702c93127d8c1f431'
